695edbb33277cf3ea56a8c5a656e071606ef4416
Ömercan Balandı <balandiomer@gmail.com>
2021-01-10 16:50:53 +0300

    Fixed responsive layout

--- src/components/Social.js
+++ src/components/Social.js
@@ -1,7 +1,7 @@
 import React from "react"
 
-import ytLogo from "../images/youtube_social_icon_red.png"
-import twLogo from "../images/Twitter_Social_Icon_Rounded_Square_Color.png"
+import YoutubeIcon from "../images/youtube_social_icon_red.png"
+import TwitterIcon from "../images/Twitter_Social_Icon_Rounded_Square_Color.png"
 import { TiledLink, TiledLinkContainer, TiledLinkIcon, SectionTitle } from '../theme'
 
 const Social = () => (
@@ -9,27 +9,27 @@ const Social = () => (
     <SectionTitle>Socials</SectionTitle>
     <TiledLinkContainer>
       <TiledLink href="https://www.youtube.com/channel/UCuXhrIE_d-Soapk2WEuGXAg">
-        <TiledLinkIcon src={ytLogo} alt="Unhandled Exception Club Youtube" />
+        <TiledLinkIcon src={YoutubeIcon} alt="Unhandled Exception Club Youtube" />
         Follow Unhandled Exception on Youtube
       </TiledLink>
 
       <TiledLink href="https://twitter.com/eralpkaraduman">
-        <TiledLinkIcon src={twLogo} alt="Eralp Karaduman Twitter" />
+        <TiledLinkIcon src={TwitterIcon} alt="Eralp Karaduman Twitter" />
         @eralpkaraduman
       </TiledLink>
 
       <TiledLink href="http://twitter.com/can_sykrfkl">
-        <TiledLinkIcon src={twLogo} alt="Can Soykarafakili Twitter" />
+        <TiledLinkIcon src={TwitterIcon} alt="Can Soykarafakili Twitter" />
         @can_sykrfkl
       </TiledLink>
 
       <TiledLink href="http://twitter.com/mertahmetgunes">
-        <TiledLinkIcon src={twLogo} alt="Mert Ahmet Gunes Twitter" />
+        <TiledLinkIcon src={TwitterIcon} alt="Mert Ahmet Gunes Twitter" />
         @mertahmetgunes
       </TiledLink>
 
       <TiledLink href="http://twitter.com/eren_ozel">
-        <TiledLinkIcon src={twLogo} alt="Eren Ozel Twitter" />
+        <TiledLinkIcon src={TwitterIcon} alt="Eren Ozel Twitter" />
         @eren_ozel
       </TiledLink>
     </TiledLinkContainer>
--- src/components/Twitch.js
+++ src/components/Twitch.js
@@ -1,45 +1,27 @@
-import React, { useState } from "react"
-import TwitchEmbedLib from "./TwitchEmbedLib"
-import TwitchEmbed from "./TwitchEmbed"
+import React from "react"
 import { TiledLinkContainer, TiledLink, TiledLinkIcon, SectionTitle } from '../theme'
 import TwitchIcon from '../images/twitch-icon.png'
-import f from "../fatureFlags"
 
 const Twitch = () => {
-  const [libLoaded, setLibLoaded] = useState(false);
-
   return (
-    <div>
+    <>
       <SectionTitle>Twitch</SectionTitle>
-      {f.embededTwitchPlayers ? (
-        <div className="twitch-container">
-          {!libLoaded ?
-            <TwitchEmbedLib loadCallback={() => setLibLoaded(true)} /> :
-            <>
-              <TwitchEmbed channelName="theBeastOfTheMiddleEast" />
-              <TwitchEmbed channelName="LeeroyCankins" />
-            </>
-          }
-        </div>
-      ) : (
-          <TiledLinkContainer>
-            <TiledLink href="https://www.twitch.tv/theBeastOfTheMiddleEast">
-              <TiledLinkIcon src={TwitchIcon} alt="TheBeastOfTheMiddleEast On Twitch" />
-                TheBeastOfTheMiddleEast On Twitch
-              </TiledLink>
-            <TiledLink href="https://www.twitch.tv/LeeroyCankins">
-              <TiledLinkIcon src={TwitchIcon} alt="LeeroyCankins On Twitch" />
-                LeeroyCankins On Twitch
-              </TiledLink>
-            <TiledLink href="https://www.twitch.tv/eren_ozel">
-              <TiledLinkIcon src={TwitchIcon} alt="Eren_Ozel On Twitch" />
-                Eren_Ozel On Twitch On Twitch
-              </TiledLink>
-          </TiledLinkContainer>
-        )}
-    </div >
+        <TiledLinkContainer>
+          <TiledLink href="https://www.twitch.tv/theBeastOfTheMiddleEast">
+            <TiledLinkIcon src={TwitchIcon} alt="TheBeastOfTheMiddleEast On Twitch" />
+              TheBeastOfTheMiddleEast On Twitch
+            </TiledLink>
+          <TiledLink href="https://www.twitch.tv/LeeroyCankins">
+            <TiledLinkIcon src={TwitchIcon} alt="LeeroyCankins On Twitch" />
+              LeeroyCankins On Twitch
+            </TiledLink>
+          <TiledLink href="https://www.twitch.tv/eren_ozel">
+            <TiledLinkIcon src={TwitchIcon} alt="Eren_Ozel On Twitch" />
+              Eren_Ozel On Twitch On Twitch
+            </TiledLink>
+        </TiledLinkContainer>
+    </>
   )
 }
-
 
 export default Twitch;
