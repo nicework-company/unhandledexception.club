e7fd27a797aea0242e733cc4bc923e6307be9e88
Ömercan Balandı <balandiomer@gmail.com>
2021-01-29 04:00:00 +0300

    Refactored styled components for theme switching

--- src/theme.js
+++ src/theme.js
@@ -1,9 +1,37 @@
 import { Link } from "gatsby"
 import React from "react"
-import styled from 'styled-components'
+import styled, { createGlobalStyle } from 'styled-components'
 
 const mediumSize = "770px"
 const smallSize = "450px"
+
+export const GlobalStyle = createGlobalStyle`
+  body {
+    --bg: #ffffff;
+    --red: #ac3232;
+    --blue: #5656ff;
+    --sky-blue: #a8dbff;
+    --white: #ffffff;
+    --black: #000000;
+    --dark: #222222;
+    --grey: #e4e4e4;
+    --soft-grey: #f5f5f5;
+    background-color: var(--bg);
+  }
+
+  body.dark {
+    --bg: #181a1b;
+    --red: #d15f5f;
+    --blue: #5656ff;
+    --sky-blue: #a8dbff;
+    --white: #e8e6e3;
+    --black: #000000;
+    --dark: #c7c7c7;
+    --grey: #383c3f;
+    --soft-grey: #1e2021;
+    background-color: var(--bg);
+  }
+`
 
 export const LayoutContainer = styled.div`
   font-family: 'Inter', sans-serif;
@@ -35,7 +63,7 @@ export const HeaderLinkImage = styled.img`
 `
 
 export const HeaderLinkTitle = styled.div`
-  color: #ac3232;
+  color: var(--red);
   padding-left: 30px;
   font-size: 56px;
   line-height: 54px;
@@ -60,31 +88,31 @@ export const HeaderLink = styled(props => <Link {...props}/>)`
   display: flex;
   align-items: center;
   text-decoration: none;
-  border: 6px solid #ac3232;
+  border: 6px solid var(--red);
   border-radius: 20px;
 
   &:hover {
     ${HeaderLinkTitle} {
-      color: #222;
+      color: var(--dark);
     }
   }
 `
 
 export const TiledLink = styled.a`
   display: flex;
-  background-color: #f5f5f5;
+  background-color: var(--soft-grey);
   width: 49%;
   align-items: center;
-  color: #222;
+  color: var(--dark);
   padding: 10px;
   margin-bottom: 10px;
   border-radius: 5px;
   text-decoration: none;
   font-size: 14px;
-  border: 1px solid #e4e4e4;
+  border: 1px solid var(--grey);
 
   &:hover {
-    background-color: #e4e4e4;
+    background-color: var(--grey);
   }
 
   @media (max-width: ${mediumSize}) {
@@ -112,6 +140,7 @@ export const TiledLinkContainer = styled.div`
 `
 
 export const SectionTitle = styled.h3`
+  color: var(--dark);
   margin: 0.4em 0 0 0;
   padding: 0;
   font-family: 'Inter', sans-serif;
@@ -130,7 +159,7 @@ export const PodcastContainer = styled.div`
 
 export const PodcastCard = styled.a`
   display: flex;
-  border: 1px solid #d8d8d8;
+  border: 1px solid var(--grey);
   justify-content: flex-start;
   align-items: flex-end;
   width: 49%;
@@ -151,12 +180,99 @@ export const PodcastCard = styled.a`
   }
 `
 
-export const PodcastCardTiltle = styled.div`
-  color: #ffffff;
-  background-color: #000000;
+export const PodcastCardTitle = styled.div`
+  color: var(--white);
+  background-color: var(--black);
   margin: 10px;
   font-size: 22px;
   padding: 2px 5px;
   border-radius: 3px;
   font-weight: bold;
+`
+
+export const TogglerLabel = styled.label`
+  position: absolute;
+  top: 10px;
+  right: 10px;
+  width: 60px;
+  height: 30px;
+  border: 3px solid var(--red);
+  border-radius: 30px;
+  overflow: hidden;
+  display: flex;
+  align-items: center;
+  cursor: pointer;
+
+  &.light {
+    background-color: var(--sky-blue);
+  }
+
+  &.dark {
+    background-color: var(--grey);
+  }
+`
+
+export const TogglerInput = styled.input`
+  display: none;
+`
+
+export const TogglerSlider = styled.div`
+  width: 20px;
+  height: 20px;
+  border-radius: 100%;
+  background-color: var(--red);
+  position: relative;
+  transition: all ease-in-out .35s;
+  top: 0px;
+
+  &.light {
+    left: 3px;
+  }
+
+  &.dark {
+    left: calc(100% - 23px);
+  }
+`
+
+export const TogglerSunImage = styled.img`
+  position: absolute;
+  transition: all ease-in-out 0.5s;
+  width: 30px;
+  filter: drop-shadow(0px 0px 5px rgb(252, 167, 38));
+
+  &.light {
+    top: -5px;
+    right: -8px;
+  }
+
+  &.dark {
+    top: -5px;
+    right: -45px;
+  }
+`
+
+export const TogglerMoonImage = styled.img`
+  position: absolute;
+  transition: all ease-in-out 0.5s;
+  width: 30px;
+  filter: drop-shadow(0px 0px 5px rgb(255, 255, 255));
+
+  &.light {
+    top: -5px;
+    left: -45px;
+  }
+
+  &.dark {
+    top: -5px;
+    left: -8px;
+  }
+`
+
+export const Footer = styled.div`
+  margin-top: 2em;
+  color: var(--dark);
+`
+
+export const FooterLink = styled.a`
+  color: var(--blue);
 `
