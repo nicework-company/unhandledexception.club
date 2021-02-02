30ed8bf607b99517d658564e960aecc1eaffba1f
Gökmen Görgen <gkmngrgn@gmail.com>
2021-02-02 23:50:29 +0100

    add pre process to resize images in build. #11

--- src/components/Social.js
+++ src/components/Social.js
@@ -1,39 +1,61 @@
 import React from "react"
-
-import YoutubeIcon from "../images/youtube_social_icon_red.png"
-import TwitterIcon from "../images/Twitter_Social_Icon_Rounded_Square_Color.png"
+import { useStaticQuery, graphql } from "gatsby"
 import { TiledLink, TiledLinkContainer, TiledLinkIcon, SectionTitle } from '../theme'
 
-const Social = () => (
-  <>
-    <SectionTitle>Socials</SectionTitle>
-    <TiledLinkContainer>
-      <TiledLink href="https://www.youtube.com/channel/UCuXhrIE_d-Soapk2WEuGXAg">
-        <TiledLinkIcon src={YoutubeIcon} alt="Unhandled Exception Club Youtube" />
-        Follow Unhandled Exception on Youtube
-      </TiledLink>
+const Social = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      youtubeIcon: file(relativePath: { eq: "youtube_social_icon_red.png" }) {
+        childImageSharp {
+          resize(height: 48) {
+            src
+          }
+        }
+      },
+      twitterIcon: file(relativePath: { eq: "Twitter_Social_Icon_Rounded_Square_Color.png" }) {
+        childImageSharp {
+          resize(height: 48) {
+            src
+          }
+        }
+      }
+    }
+  `)
+  const youtubeIcon = data.youtubeIcon.childImageSharp.resize.src
+  const twitterIcon = data.twitterIcon.childImageSharp.resize.src
 
-      <TiledLink href="https://twitter.com/eralpkaraduman">
-        <TiledLinkIcon src={TwitterIcon} alt="Eralp Karaduman Twitter" />
-        {'@eralpkaraduman'}
-      </TiledLink>
+  return (
+    <>
+      <SectionTitle>Socials</SectionTitle>
 
-      <TiledLink href="http://twitter.com/can_sykrfkl">
-        <TiledLinkIcon src={TwitterIcon} alt="Can Soykarafakili Twitter" />
-        {'@can_sykrfkl'}
-      </TiledLink>
+      <TiledLinkContainer>
+        <TiledLink href="https://www.youtube.com/channel/UCuXhrIE_d-Soapk2WEuGXAg">
+          <TiledLinkIcon src={youtubeIcon} alt="Unhandled Exception Club Youtube" />
+          Follow Unhandled Exception on Youtube
+        </TiledLink>
 
-      <TiledLink href="http://twitter.com/mertahmetgunes">
-        <TiledLinkIcon src={TwitterIcon} alt="Mert Ahmet Gunes Twitter" />
-        {'@mertahmetgunes'}
-      </TiledLink>
+        <TiledLink href="https://twitter.com/eralpkaraduman">
+          <TiledLinkIcon src={twitterIcon} alt="Eralp Karaduman Twitter" />
+          {'@eralpkaraduman'}
+        </TiledLink>
 
-      <TiledLink href="http://twitter.com/eren_ozel">
-        <TiledLinkIcon src={TwitterIcon} alt="Eren Ozel Twitter" />
-        {'@eren_ozel'}
-      </TiledLink>
-    </TiledLinkContainer>
-  </>
-)
+        <TiledLink href="http://twitter.com/can_sykrfkl">
+          <TiledLinkIcon src={twitterIcon} alt="Can Soykarafakili Twitter" />
+          {'@can_sykrfkl'}
+        </TiledLink>
+
+        <TiledLink href="http://twitter.com/mertahmetgunes">
+          <TiledLinkIcon src={twitterIcon} alt="Mert Ahmet Gunes Twitter" />
+          {'@mertahmetgunes'}
+        </TiledLink>
+
+        <TiledLink href="http://twitter.com/eren_ozel">
+          <TiledLinkIcon src={twitterIcon} alt="Eren Ozel Twitter" />
+          {'@eren_ozel'}
+        </TiledLink>
+      </TiledLinkContainer>
+    </>
+  )
+}
 
 export default Social
