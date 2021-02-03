c546eaa7a5ebd615221599dfd1720b6b3a4c4a11
Ömercan Balandı <balandiomer@gmail.com>
2021-02-03 20:24:09 +0300

    Deferred TogglerLabel rendering at build time

--- src/components/ThemeToggleButton.js
+++ src/components/ThemeToggleButton.js
@@ -17,16 +17,19 @@ const ThemeToggleButton = () => {
       {({ theme, toggleTheme }) => (
         <>
           <GlobalStyle theme={theme} />
-          <TogglerLabel className={theme}>
-            <TogglerInput
-              type='checkbox'
-              onChange={e => toggleTheme(e.target.checked ? 'dark' : 'light')}
-              checked={theme === 'dark'}
-            />
-            <TogglerSunImage src={SunImage} className={theme} alt="Light" />
-            <TogglerMoonImage src={MoonImage} className={theme} alt="Dark" />
-            <TogglerSlider className={theme}/>
-          </TogglerLabel>
+          {
+            theme !== null &&
+            <TogglerLabel className={theme}>
+              <TogglerInput
+                type='checkbox'
+                onChange={e => toggleTheme(e.target.checked ? 'dark' : 'light')}
+                checked={theme === 'dark'}
+              />
+              <TogglerSunImage src={SunImage} className={theme} alt="Light" />
+              <TogglerMoonImage src={MoonImage} className={theme} alt="Dark" />
+              <TogglerSlider className={theme}/>
+            </TogglerLabel>
+          }
         </>
       )}
   </ThemeToggler>
