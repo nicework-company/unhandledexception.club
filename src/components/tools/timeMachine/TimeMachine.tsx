6fd4180633b2774fe8fbc3b33aaaeb0218829e63
Eralp Karaduman <eralp@eralpkaraduman.com>
2021-02-04 23:55:08 +0200

    Time machine text input

--- src/components/tools/timeMachine/TimeMachine.tsx
+++ src/components/tools/timeMachine/TimeMachine.tsx
@@ -1,14 +1,10 @@
 import * as React from "react"
+import { useState } from "react"
 
 enum Tokens {
   TIMES = "{{TIMES}}",
   STREAM_LINK = "{{STREAM_LINK}}",
 }
-
-const postTemplate = `Bu aksam pregremning yayini olacaktir.
-${Tokens.TIMES}
-${Tokens.STREAM_LINK}
-`
 
 const timeListTemplate = `
 {{helsinki/finland}}
@@ -19,14 +15,33 @@ const generateTimes = template => {
   return template
 }
 
-const generatePost = template => {
+const generateOutput = postTemplate => {
   return postTemplate.split(Tokens.TIMES).join(generateTimes(timeListTemplate))
 }
 
 const TimeMachine = () => {
+  const [postTemplate, setPostTemplate] = useState<string>(
+    `Bu aksam pregremning yayini olacaktir. ${Tokens.TIMES} ${Tokens.STREAM_LINK}`
+  )
+  const handleTemplateChange = e => setPostTemplate(e.target.value)
   return (
     <div>
-      <pre>{generatePost(postTemplate)}</pre>
+      <textarea
+        id="input"
+        name="story"
+        rows={5}
+        cols={33}
+        value={postTemplate}
+        onChange={handleTemplateChange}
+      />
+      <textarea
+        readOnly
+        id="output"
+        name="story"
+        rows={5}
+        cols={33}
+        value={generateOutput(postTemplate)}
+      />
     </div>
   )
 }
