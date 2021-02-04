8ab0d9cf3ce13853f3df65a38e0a31801ecd9581
Gökmen Görgen <gkmngrgn@gmail.com>
2021-02-04 21:53:55 +0100

    add pre process to resize images in build. #11

--- src/components/TopLinks.js
+++ src/components/TopLinks.js
@@ -1,21 +1,45 @@
 import React from "react"
-
-import ytLogo from "../images/youtube_social_icon_red.png"
-import dcLogo from "../images/Discord-Logo-Color.png"
+import { useStaticQuery, graphql } from "gatsby"
 import { TiledLinkContainer, TiledLink, TiledLinkIcon } from '../theme'
 
-const TopLinks = () => (
-  <TiledLinkContainer>
-    <TiledLink href="https://www.youtube.com/channel/UCuXhrIE_d-Soapk2WEuGXAg/live">
-      <TiledLinkIcon src={ytLogo} alt="Unhandled Exception Club Youtube Live" />
-      Watch Live On Youtube
-    </TiledLink>
 
-    <TiledLink href="http://discord.gg/4GgCG5C">
-      <TiledLinkIcon src={dcLogo} alt="Unhandled Exception Club Discord" />
-      Join Discord
-    </TiledLink>
-  </TiledLinkContainer>
-)
+const TopLinks = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      youtubeIcon: file(relativePath: { eq: "youtube_social_icon_red.png" }) {
+        childImageSharp {
+          resize(height: 48) {
+            src
+          }
+        }
+      },
+      discordIcon: file(relativePath: { eq: "Discord-Logo-Color.png" }) {
+        childImageSharp {
+          resize(height: 48) {
+            src
+          }
+        }
+      }
+    }
+  `)
+  const youtubeIcon = data.youtubeIcon.childImageSharp.resize.src
+  const discordIcon = data.discordIcon.childImageSharp.resize.src
+
+  return (
+    <>
+      <TiledLinkContainer>
+        <TiledLink href="https://www.youtube.com/channel/UCuXhrIE_d-Soapk2WEuGXAg/live">
+          <TiledLinkIcon src={youtubeIcon} alt="Unhandled Exception Club Youtube Live" />
+          Watch Live On Youtube
+        </TiledLink>
+
+        <TiledLink href="http://discord.gg/4GgCG5C">
+          <TiledLinkIcon src={discordIcon} alt="Unhandled Exception Club Discord" />
+          Join Discord
+        </TiledLink>
+      </TiledLinkContainer>
+    </>
+  )
+}
 
 export default TopLinks
--- src/components/Twitch.js
+++ src/components/Twitch.js
@@ -1,29 +1,47 @@
 import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
 import { TiledLinkContainer, TiledLink, TiledLinkIcon, SectionTitle } from '../theme'
-import TwitchIcon from '../images/twitch-icon.png'
+
 
 const Twitch = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      twitchIcon: file(relativePath: { eq: "twitch-icon.png" }) {
+        childImageSharp {
+          resize(height: 48) {
+            src
+          }
+        }
+      }
+    }
+  `)
+  const twitchIcon = data.twitchIcon.childImageSharp.resize.src
+
   return (
     <>
       <SectionTitle>Twitch</SectionTitle>
-        <TiledLinkContainer>
-          <TiledLink href="https://www.twitch.tv/ErikThePlum">
-            <TiledLinkIcon src={TwitchIcon} alt="ErikThePlum On Twitch" />
-            ErikThePlum On Twitch
-          </TiledLink>
-          <TiledLink href="https://www.twitch.tv/theBeastOfTheMiddleEast">
-            <TiledLinkIcon src={TwitchIcon} alt="TheBeastOfTheMiddleEast On Twitch" />
-              TheBeastOfTheMiddleEast On Twitch
-          </TiledLink>
-          <TiledLink href="https://www.twitch.tv/LeeroyCankins">
-            <TiledLinkIcon src={TwitchIcon} alt="LeeroyCankins On Twitch" />
-              LeeroyCankins On Twitch
-          </TiledLink>
-          <TiledLink href="https://www.twitch.tv/eren_ozel">
-            <TiledLinkIcon src={TwitchIcon} alt="Eren_Ozel On Twitch" />
-              Eren_Ozel On Twitch
-          </TiledLink>
-        </TiledLinkContainer>
+
+      <TiledLinkContainer>
+        <TiledLink href="https://www.twitch.tv/ErikThePlum">
+          <TiledLinkIcon src={twitchIcon} alt="ErikThePlum On Twitch" />
+          ErikThePlum On Twitch
+        </TiledLink>
+
+        <TiledLink href="https://www.twitch.tv/theBeastOfTheMiddleEast">
+          <TiledLinkIcon src={twitchIcon} alt="TheBeastOfTheMiddleEast On Twitch" />
+            TheBeastOfTheMiddleEast On Twitch
+        </TiledLink>
+
+        <TiledLink href="https://www.twitch.tv/LeeroyCankins">
+          <TiledLinkIcon src={twitchIcon} alt="LeeroyCankins On Twitch" />
+            LeeroyCankins On Twitch
+        </TiledLink>
+
+        <TiledLink href="https://www.twitch.tv/eren_ozel">
+          <TiledLinkIcon src={twitchIcon} alt="Eren_Ozel On Twitch" />
+            Eren_Ozel On Twitch
+        </TiledLink>
+      </TiledLinkContainer>
     </>
   )
 }
