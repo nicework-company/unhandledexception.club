628b3eaf8fbe68bcf3831ae990add29114caefbd
Eralp Karaduman <eralpkaraduman@gmail.com>
2021-02-24 16:49:04 +0200

    Move youtube link down, add flexible layout (#36)

--- src/components/TopLinks.js
+++ src/components/TopLinks.js
@@ -1,18 +1,10 @@
 import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
-import { TiledLinkContainer, TiledLink, TiledLinkIcon } from '../theme'
-
+import { TiledLinkContainer, TiledLink, TiledLinkIcon } from "../theme"
 
 const TopLinks = () => {
   const data = useStaticQuery(graphql`
     query {
-      youtubeIcon: file(relativePath: { eq: "youtube_social_icon_red.png" }) {
-        childImageSharp {
-          resize(height: 48) {
-            src
-          }
-        }
-      },
       discordIcon: file(relativePath: { eq: "Discord-Logo-Color.png" }) {
         childImageSharp {
           resize(height: 48) {
@@ -22,20 +14,17 @@ const TopLinks = () => {
       }
     }
   `)
-  const youtubeIcon = data.youtubeIcon.childImageSharp.resize.src
   const discordIcon = data.discordIcon.childImageSharp.resize.src
 
   return (
     <>
       <TiledLinkContainer>
-        <TiledLink href="https://www.youtube.com/channel/UCuXhrIE_d-Soapk2WEuGXAg/live">
-          <TiledLinkIcon src={youtubeIcon} alt="Unhandled Exception Club Youtube Live" />
-          Watch Live On Youtube
-        </TiledLink>
-
         <TiledLink href="http://discord.gg/4GgCG5C">
-          <TiledLinkIcon src={discordIcon} alt="Unhandled Exception Club Discord" />
-          Join Discord
+          Join Unhandled Exception Club Discord
+          <TiledLinkIcon
+            src={discordIcon}
+            alt="Unhandled Exception Club Discord"
+          />
         </TiledLink>
       </TiledLinkContainer>
     </>
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import TopLinks from "../components/TopLinks"
-import Twitch from "../components/Twitch"
+import Streams from "../components/Streams"
 import Podcast from "../components/Podcast"
 import Social from "../components/Social"
 
@@ -10,10 +10,10 @@ const IndexPage = () => {
   return (
     <Layout>
       <SEO title="Home" />
-        <TopLinks />
-        <Twitch />
-        <Podcast />
-        <Social />
+      <TopLinks />
+      <Streams />
+      <Podcast />
+      <Social />
     </Layout>
   )
 }
